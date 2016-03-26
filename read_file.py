9d3374d56a20955f42da9975736e24d5af7c44d9
Matthew Hansen <han11077@byui.edu>
2016-03-26 17:28:13 -0600

    read lungs in read_file.py is now working

--- read_file.py
+++ read_file.py
@@ -2,10 +2,31 @@ import numpy as np
 
 
 
-def read_auto_data():
-	filename = "auto-mpg.data"
+def read_lungs():
+	filename = "lung-cancer.data"
 	the_file = open(filename, "r")
-	target_column = 8
+	target_column = 0
+	data = np.zeros((32,56))
+	targets = []
+	row = 0
+
+	for line in the_file:
+		temp_string = line.split(",")
+		targets.append(temp_string[target_column].rstrip('\n'))
+		temp_string.pop(target_column)
+		column = 0
+		for split in temp_string:
+			if split == "?":
+				if column == 3:
+					split = "-1"
+				else:
+					split = "4"
+			data[row][column] = float(split)
+			column += 1
+		row += 1
+
+	return data, targets
+
 
 
 def read_abalone():
@@ -65,7 +86,7 @@ def read_letters():
 
 
 def main():
-	data, targets = read_abalone()
+	data, targets = read_lungs()
 	print(data)
 	print(targets)
 
