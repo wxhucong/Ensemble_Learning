f34b07d76e9b50bda208fa76da0daa612ee2bab3
diabloazul14 <han11077@byui.edu>
2016-03-25 18:25:15 -0600

    Decision tree works now

--- decision_tree.py
+++ decision_tree.py
@@ -1,7 +1,19 @@
 from sklearn.datasets import load_iris
 from sklearn import tree
+from sklearn.metrics import accuracy_score
 iris = load_iris()
 clf = tree.DecisionTreeClassifier(criterion='entropy')
 clf = clf.fit(iris.data, iris.target)
-answer = clf.predict([[1, 1]])
+print("Iris accuracy: " + str(clf.score(iris.data, iris.target)) + "%")
+
+
+
+
+
+
+
+
+
+
+
 
