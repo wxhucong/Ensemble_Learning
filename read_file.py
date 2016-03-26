96e5d45e6657737aa6491ab2882d0c769cb2c232
Matthew Hansen <han11077@byui.edu>
2016-03-26 16:36:35 -0600

    read_abalone is now working

--- read_file.py
+++ read_file.py
@@ -8,10 +8,34 @@ def read_auto_data():
 	target_column = 8
 
 
-def read_flare():
-	filename = "flare.data"
+def read_abalone():
+	filename = "abalone.data"
 	the_file = open(filename, "r")
 	target_column = 0
+	data = np.zeros((4177,8))
+	targets = []
+	row = 0
+
+	for line in the_file:
+		temp_string = line.split(",")
+		targets.append(temp_string[8].rstrip('\n'))
+		temp_string.pop(8)
+		column = 0
+		for split in temp_string:
+			split.rstrip('\n')
+			if column == 0:
+				if split == 'M':
+					split = "0"
+				elif split == 'F':
+					split = "1"
+				else:
+					split = "2"
+
+			data[row][column] = float(split)
+			column += 1
+		row += 1
+
+	return data, targets
 
 
 def read_letters():
@@ -41,7 +65,7 @@ def read_letters():
 
 
 def main():
-	data, targets = read_letters()
+	data, targets = read_abalone()
 	print(data)
 	print(targets)
 
