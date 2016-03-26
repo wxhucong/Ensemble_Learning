3f330b1d899bbb8b0d350acb1754d38ddd78babe
Matthew Hansen <han11077@byui.edu>
2016-03-26 16:09:38 -0600

    read_letters is now working

--- read_file.py
+++ read_file.py
@@ -1,5 +1,49 @@
-def read_file(filename, target_column):
-    the_file = open(filename, "r")
-    data = []
-    targets = []
-    
+import numpy as np
+
+
+
+def read_auto_data():
+	filename = "auto-mpg.data"
+	the_file = open(filename, "r")
+	target_column = 8
+
+
+def read_flare():
+	filename = "flare.data"
+	the_file = open(filename, "r")
+	target_column = 0
+
+
+def read_letters():
+	filename = "letter-recognition.data"
+	the_file = open(filename, "r")
+	target_column = 0
+	data = np.zeros((20000,16))
+	targets = []
+	row = 0
+
+
+	for line in the_file:
+		temp_string = line.split(",")
+		targets.append(temp_string[0])
+		temp_string.pop(0)
+		column = 0
+		for split in temp_string:
+			split.rstrip('\n')
+			data[row][column] = int(split)
+			column += 1
+		row += 1
+
+	return data, targets
+
+
+
+
+
+def main():
+	data, targets = read_letters()
+	print(data)
+	print(targets)
+
+
+main()
