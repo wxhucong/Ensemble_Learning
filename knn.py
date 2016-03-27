cca3a4b12c35f243e91f22f5efe6de46ed2f4a80
Matthew Hansen <han11077@byui.edu>
2016-03-26 18:27:25 -0600

    Finished classifiers

--- knn.py
+++ knn.py
@@ -1,12 +1,25 @@
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cross_validation import cross_val_score
 import read_file as rf
 
 def main():
 	data, targets = rf.read_letters()
-	clf = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(data)
+	clf = KNeighborsClassifier(n_neighbors=2, algorithm='ball_tree')
 	scores = cross_val_score(clf, data, targets)
 	print("KNN_Letters: ", end="")
 	print(scores.mean() * 100)
+
+	data, targets = rf.read_abalone()
+	clf = KNeighborsClassifier(n_neighbors=2, algorithm='ball_tree')
+	scores = cross_val_score(clf, data, targets)
+	print("KNN_Abalone: ", end="")
+	print(scores.mean() * 100)
+
+	data, targets = rf.read_lungs()
+	clf = KNeighborsClassifier(n_neighbors=2, algorithm='ball_tree')
+	scores = cross_val_score(clf, data, targets)
+	print("KNN_Lungs: ", end="")
+	print(scores.mean() * 100)
+
 
 main()
